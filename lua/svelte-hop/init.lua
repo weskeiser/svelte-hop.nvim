5ecfcbb8fa532ee7a520f95e0c9498fd8516d755
Wes Keiser <wes@weskeiser.no>
2023-10-03 17:29:30 +0200

    Plugin is now functional

--- lua/svelte-hop/init.lua
+++ lua/svelte-hop/init.lua
@@ -1,7 +1,10 @@
 local M = {}
 local utils = require("svelte-hop.utils")
+local config = require("svelte-hop.config")
+local usercmd = vim.api.nvim_create_user_command
+local autocmd = vim.api.nvim_create_autocmd
 
-M.commands_short = {
+local command_args_filename_mappings = {
 	["ps"] = "+page.svelte",
 	["p"] = "+page.ts",
 	["psv"] = "+page.server.ts",
@@ -14,48 +17,67 @@ M.commands_short = {
 	["e"] = "+error.ts",
 }
 
-function M.navigate_sibling_by_name(fname)
-	local fpath = vim.fn.fnamemodify(vim.fn.expand("%"), ":p:h") .. "/" .. fname
-	if utils.file_exists(fpath) then
-		vim.cmd("e " .. fpath)
+local function create_svop_activate_autocommand()
+	vim.api.nvim_create_autocmd("BufAdd", {
+		pattern = config.pattern,
+		callback = function()
+			print("BufAdd autocmd ran")
+			M.enable_and_activate_svop()
+		end,
+		once = true,
+		group = vim.api.nvim_create_augroup("Svop", { clear = true }),
+	})
+end
+
+function M.enable_and_activate_svop()
+	print("enable_and_activate_svop ran")
+	config.update({ enabled = true, active = true })
+	utils.map_svop_keys(config)
+end
+
+function M.enable_svop()
+	if utils.is_sveltelike_dir() then
+		print("enable_svop ran and is sveltelike dir")
+		M.enable_and_activate_svop()
+	else
+		print("enable_svop ran and is not sveltelike dir")
+		config.update({ enabled = true })
+		create_svop_activate_autocommand()
 	end
 end
 
-function M.setup(config)
-	if not config then
-		config = {}
+function M.setup(user_config)
+	if not user_config then
+		user_config = {}
 	end
 
-	local merged_config = utils.merge_tables({
-		keymaps = {
-			["+page.svelte"] = "<leader>1",
-			["+page.ts"] = "<leader>3",
-			["+page.server.ts"] = "<leader>5",
+	config.update(user_config)
 
-			["+layout.svelte"] = "<leader>2",
-			["+layout.ts"] = "<leader>4",
-			["+layout.server.ts"] = "<leader>6",
-
-			["+server.ts"] = "<leader>7",
-			["+error.ts"] = "<leader>0",
-		},
-	}, utils.expand_dir(config))
-
-	local map = vim.keymap.set
-
-	for filename, keymap in pairs(merged_config.keymaps) do
-		map("n", keymap, function()
-			M.navigate_sibling_by_name(filename)
-		end)
+	if config.enabled and utils.is_sveltelike_dir() then
+		print("setup ran, svop is enabled and is_sveltelike_dir")
+		M.enable_and_activate_svop()
+	else
+		if config.enabled then
+			print("setup ran, and svop is enabled")
+			create_svop_activate_autocommand()
+		end
 	end
 
-	vim.api.nvim_create_user_command("Svop", function(opts)
-		local fname = M.commands_short[opts.fargs[1]]
-		M.navigate_sibling_by_name(fname)
+	usercmd("Svop", function(opts)
+		local command_arg = opts.fargs[1]
+		utils.open_sibling_by_filename(command_args_filename_mappings[command_arg])
 	end, {
 		nargs = 1,
-		desc = "Svelte-Hop",
+		desc = "Svelte-Hop Navigator",
 	})
+
+	usercmd("SvopEnable", function()
+		M.enable_svop()
+	end, {})
+
+	usercmd("SvopActivate", function()
+		M.enable_and_activate_svop()
+	end, {})
 end
 
 return M
