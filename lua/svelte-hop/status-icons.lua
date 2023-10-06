5bca838ef61bb621f6568c9e6cf3984c0f523951
Wes Keiser <wes@weskeiser.no>
2023-10-06 02:42:49 +0200

    Refactor status icons
    BREAKING CHANGES

--- lua/svelte-hop/status-icons.lua
+++ lua/svelte-hop/status-icons.lua
@@ -1,58 +1,66 @@
-local utils = require("svelte-hop.utils")
 local config = require("svelte-hop.config")
 local setHl = function(k, v)
 	vim.api.nvim_set_hl(0, k, v)
 end
 
-setHl("SvopGroup1", { fg = "#77cc77" })
-setHl("SvopGroup1Missing", { fg = "#777777" })
-setHl("SvopGroup2", { fg = "#7777cc" })
-setHl("SvopGroup2Missing", { fg = "#777777" })
-setHl("SvopGroup3", { fg = "#cccccc" })
-setHl("SvopGroup3Missing", { fg = "#777777" })
+setHl("Svop1", { fg = "#b98282" })
+setHl("Svop2", { fg = "#6696cc" })
+setHl("Svop3", { fg = "#96cc66" })
+setHl("Svop4", { fg = "#8381a2" })
+setHl("CurrentSvop1", { fg = "#b98282", sp = "#cf9ad7", underline = true })
+setHl("CurrentSvop2", { fg = "#86b6ec", sp = "#a6d6ff", underline = true })
+setHl("CurrentSvop3", { fg = "#96cc66", sp = "#b6ec86", underline = true })
+setHl("CurrentSvop4", { fg = "#8381a2", sp = "#a3a1c2", underline = true })
+setHl("MissingSvop", { fg = "#555555" })
 
-local fnames_icons = {
-	["+page.svelte"] = " ❶ ",
-	["+page.ts"] = " ❷ ",
-	["+page.server.ts"] = " ❸  ",
-
-	["+layout.svelte"] = " ❶ ",
-	["+layout.ts"] = " ❷ ",
-	["+layout.server.ts"] = " ❸  ",
-
-	["+server.ts"] = " ❹ ",
-	["+error.ts"] = " ❺",
-}
-
+--
 local M = {}
 
 M.status_autocmd_id = nil
 
-function M.set_buflocal_statusline(buf)
-	local froot = vim.fn.fnamemodify(vim.fn.expand("%"), ":p:h") .. "/"
+function M.set_buf_svopstatus(buf)
+	local current_path = vim.fn.expand("%")
+	local file_root = vim.fn.fnamemodify(current_path, ":p:h") .. "/"
 
-	print(buf)
 	local statusline = ""
-	for i, fname in ipairs(require("svelte-hop").route_files) do
-		local group_no = 1
 
-		if i > 6 then
-			group_no = 3
+	local function append_icon_statusline(hl_name, groupnr, icon, space_after)
+		if not groupnr then
+			groupnr = ""
+		end
+
+		statusline = statusline .. "%#" .. hl_name .. groupnr .. "#" .. icon .. "%#Normal#" .. space_after
+	end
+
+	for i, fname in ipairs(require("svelte-hop").route_files) do
+		local group_no = 4
+		local space_after = " "
+
+		if i == 1 or i == 4 then
+			group_no = 1
 		else
-			if i > 3 then
+			if i == 3 or i == 6 then
 				group_no = 2
+				space_after = "  "
+			else
+				if i == 2 or i == 5 then
+					group_no = 3
+				end
 			end
 		end
 
-		local hl = "%#SvopGroup" .. group_no
-
-		if utils.file_exists(froot .. fname) then
-			hl = hl .. "#"
+		if vim.fn.fnamemodify(current_path, ":t") == fname then
+			-- if is current file
+			append_icon_statusline("CurrentSvop", group_no, "◉", space_after)
 		else
-			hl = hl .. "Missing#"
+			if require("svelte-hop.utils").file_exists(file_root .. fname) then
+				-- if file exists
+				append_icon_statusline("Svop", group_no, "●", space_after)
+			else
+				-- if file does not exists
+				append_icon_statusline("MissingSvop", nil, "●", space_after)
+			end
 		end
-
-		statusline = statusline .. hl .. fnames_icons[fname]
 	end
 
 	vim.b[buf].svopstatus = statusline
@@ -64,13 +72,13 @@ function M.enable_status_icons()
 	M.status_autocmd_id = vim.api.nvim_create_autocmd({ "BufEnter", "BufDelete" }, {
 		pattern = config.activation_pattern,
 		callback = function(event)
-			M.set_buflocal_statusline(event.buf)
+			M.set_buf_svopstatus(event.buf)
 		end,
 		group = vim.api.nvim_create_augroup("SvopStatus", { clear = true }),
 	})
 
 	local buf = vim.api.nvim_get_current_buf()
-	M.set_buflocal_statusline(buf)
+	M.set_buf_svopstatus(buf)
 end
 
 function M.disable_status_icons()
